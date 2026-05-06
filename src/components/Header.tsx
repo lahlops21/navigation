9af7a024779f80415126cce22b9c3cc8d103c213
Lais Lopes <laisllopess21@gmail.com>
2026-05-06 10:26:18 -0300

    inicio da montagem da tela

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,19 @@
 import { View } from "react-native";
 
+// React.ReactNode -> É um tipo que represent qualquer coisa que possa ser 
+// renderizada pelo React, como elementos, componentes, strings, numeros, etc
 
-export default function Header(){
+export default function Header({children}: {children: React.ReactNode}){
+   return(
    <View
    style = {{
     width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-between', // direção horizontal
     alignItems: 'center', // direção vertical
-    marginBottom: 54
-   }}
-   
-   
-   >
-        
+    marginBottom: 54,
+   }}>
+    {children}
    </View>
+   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,16 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import Header from "../components/Header";
+import ButtonIcon from "../components/ButtonIcon";
+
+//Tela inicial 
 
 export default function Home(){
     return(
     <View style= {{flex:1, padding:32, paddingTop:54}}>
-        <Text>
-            Home
-        </Text>
+        <Header>
+            <ButtonIcon
+            nameIcon="add-circle"
+            />
+        </Header>
     </View>)
 }
